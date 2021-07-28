f735aadea6bd6b3fd63a350ada13faf7b84b715c
Akhilesh Sharma <47084013+Dragnoid99@users.noreply.github.com>
2021-07-28 21:27:06 +0530

    Update containerpackingalgo.py

--- kubernetes/qosscheduler/scheduler/code/containerpackingalgo.py
+++ kubernetes/qosscheduler/scheduler/code/containerpackingalgo.py
@@ -8,6 +8,7 @@ import pickle
 from kubernetes.client import configuration
 import random
 
+
 # Container packing algorithm based on CPU
 def container_packing_algorithm(pod, nodes, cpu_req, cpu_cap, cpu_used):
 
@@ -22,39 +23,39 @@ def container_packing_algorithm(pod, nodes, cpu_req, cpu_cap, cpu_used):
   # Divide the nodes in 3 categories, low usage nodes, medium usage nodes and high usage nodes based on their cpu utilization
   for node in nodes:
     try:
-      util = cpu_used[node]/cpu_cap[node]
-      if util<=low:
+      util = cpu_used[node] / cpu_cap[node]
+      if util <= low:
         low_nodes[node] = util
-      elif util<=high:
+      elif util <= high:
         medium_nodes[node] = util
-      else :
+      else:
         high_nodes[node] = util
-    
+
     except:
       pass
-  
+
   best_node = ""
 
   max_util = 0
 
   # First try to select node from medium utilization nodes
   for node in medium_nodes:
-    if medium_nodes[node] > max_util :
+    if medium_nodes[node] > max_util:
       max_util = medium_nodes[node]
       best_node = node
 
   # If there is no node available in medium utilization node, then try to select from low utilization nodes
   if best_node == "":
     for node in low_nodes:
-      if low_nodes[node] > max_util :
+      if low_nodes[node] > max_util:
         max_util = low_nodes[node]
         best_node = node
 
-  # If there is no node available in low, medium utilization node, then try to select from high utilization nodes    
+  # If there is no node available in low, medium utilization node, then try to select from high utilization nodes
   if best_node == "":
     min_util = 1e20
     for node in high_nodes:
-      if high_nodes[node] < min_util :
+      if high_nodes[node] < min_util:
         min_util = high_nodes[node]
         best_node = node
 
