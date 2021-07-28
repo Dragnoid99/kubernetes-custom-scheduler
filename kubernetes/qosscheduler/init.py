f21c77b10a1163c135ee83cfcb1fa3415bed8d12
Akhilesh Sharma <47084013+Dragnoid99@users.noreply.github.com>
2021-07-29 00:31:40 +0530

    Update init.py

--- kubernetes/qosscheduler/init.py
+++ kubernetes/qosscheduler/init.py
@@ -17,9 +17,22 @@ dbfile = open('scheduler/files/pods_active_pickle', 'wb')
 pickle.dump(a,dbfile)
 dbfile.close()
 
-
 dbfile = open('scheduler/files/pod_deletion_pickle', 'wb')
 pickle.dump(a,dbfile)
 dbfile.close()
 
+dbfile = open('scheduler/files/deployment_qos_metric', 'wb')
+pickle.dump(a,dbfile)
+dbfile.close()
 
+dbfile = open('../files/deployment_run_time', 'wb')
+pickle.dump(a, dbfile)
+dbfile.close()
+
+dbfile = open('../files/deployment_pause_time', 'wb')
+pickle.dump(a, dbfile)
+dbfile.close()
+
+dbfile = open('../files/deployment_qos', 'wb')
+pickle.dump(a, dbfile)
+dbfile.close()
