03f44e42af17204e3589c04456042546906082e8
Akhilesh Sharma <47084013+Dragnoid99@users.noreply.github.com>
2021-07-28 21:28:13 +0530

    Update pendingqueueops.py

--- kubernetes/qosscheduler/scheduler/code/pendingqueueops.py
+++ kubernetes/qosscheduler/scheduler/code/pendingqueueops.py
@@ -7,11 +7,12 @@ from dateutil import parser
 import pickle
 from kubernetes.client import configuration
 import random
-from helpers import cpu_convert, mem_convert, get_pod_object, get_deployment, is_valid_pod, is_valid_node, is_smaller, is_equal, time_difference
+from helpers import cpu_convert, mem_convert, get_pod_object, get_deployment, is_valid_pod, is_valid_node
 
 # Import Kubernetes configurations
 config.load_incluster_config()
 v1 = client.CoreV1Api()
+
 
 # Function to create pending queue
 def create_pending_queue():
@@ -22,28 +23,25 @@ def create_pending_queue():
   # Watch the events
   w = watch.Watch()
 
-  curr_time = datetime.datetime.now(tz = IST)
-
-  for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds = 1):
+  for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds=1):
     # Check if the pod is scheduled or not
-    if event['object'].status.phase == "Pending" and event['object'].spec.node_name == None:
+    if event['object'].status.phase == 'Pending' and event['object'].spec.node_name == None and event['object'].spec.scheduler_name == "qos-scheduler":
       # If the pod is valid pod the put it in pending queue
       if is_valid_pod(event['object'].metadata.name) == True:
         pending_queue.append(event['object'])
 
-    if time_difference(datetime.datetime.now(tz = IST), curr_time)>1:
-      break
-
   return pending_queue
 
 
-# Sort the pending queue 
+# Sort the pending queue
 def sort_pending_queue(pending_queue, pod_qos):
   # Currently sorting algorithm is Bubble Sort
   for i in range(len(pending_queue)):
-    for j in range(len(pending_queue)-1):
-      if pod_qos[get_deployment(pending_queue[j].metadata.name)] > pod_qos[get_deployment(pending_queue[j+1].metadata.name)]:
-        temp = pending_queue[j+1]
-        pending_queue[j+1] = pending_queue[j]
+    for j in range(len(pending_queue) - 1):
+      if pod_qos[get_deployment(
+          pending_queue[j].metadata.name)] > pod_qos[get_deployment(
+              pending_queue[j + 1].metadata.name)]:
+        temp = pending_queue[j + 1]
+        pending_queue[j + 1] = pending_queue[j]
         pending_queue[j] = temp
   return pending_queue
