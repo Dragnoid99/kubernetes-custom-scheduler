ba0171eac4548f415970ec2edcc9481643b93113
Akhilesh Sharma <47084013+Dragnoid99@users.noreply.github.com>
2021-07-28 21:25:23 +0530

    Update calculateqos.py

--- kubernetes/qosscheduler/scheduler/code/calculateqos.py
+++ kubernetes/qosscheduler/scheduler/code/calculateqos.py
@@ -8,45 +8,87 @@ from dateutil import parser
 import pickle
 from kubernetes.client import configuration
 import random
-from helpers import cpu_convert, mem_convert, get_pod_object, get_deployment, is_valid_pod, is_valid_node, is_smaller, is_equal, time_difference
+from helpers import cpu_convert, mem_convert, get_pod_object, get_deployment, is_valid_pod, is_valid_node
 from calculateqoshelper import get_pod_deployments, get_pod_creation_timestamps, get_deployment_slos, get_pod_deletion_timestamps
 
 # Import Kubernetes configurations
 config.load_incluster_config()
 v1 = client.CoreV1Api()
 
+# Returns the logs of all events of the cluster
+def _generate_logs():
+  pod_namespace = "kube-system"
+  pods = v1.list_namespaced_pod('kube-system')
+
+  # Obtain the pod_name of the pod of deployment eventrouter
+  for pod in pods.items:
+    if len(pod.metadata.name)>=11 and pod.metadata.name[:11] == "eventrouter":
+      pod_name = pod.metadata.name
+
+  # Read the logs of the pod
+  logs = v1.read_namespaced_pod_log(name=pod_name, namespace=pod_namespace)
+  log_string = ""
+
+  cnt_level = 0
+  count_entry = 0
+  tmp_json_list = []
+  json_list = []
+  tmp_list = []
+
+  # Parse the logs of eventrouter as string
+  for i in range(len(logs)):
+    flag = 0
+
+    if logs[i] == '{':
+      cnt_level += 1
+
+    if cnt_level != 0:
+      flag = 1
+      log_string += logs[i]
+
+    if logs[i] == '}':
+      cnt_level -= 1
+
+    if cnt_level == 0 and flag == 1:
+      # In this case, one of the entries of logs has been parsed and it's corresponding string form is stored in log_string
+      json_obj = json.loads(log_string)
+      log_string = ""
+      json_obj2 = {}
+      json_obj2['action'] = json_obj['event']['reason']
+      json_obj2['involvedObject'] = json_obj['event']['involvedObject']['name']
+      json_obj2['timestamp'] = json_obj['event']['firstTimestamp']
+      tmp_list.append((parser.parse(json_obj['event']['firstTimestamp']), count_entry))
+
+      tmp_json_list.append(json_obj2)
+      count_entry += 1
+
+  sorted(tmp_list)
+
+  for i in range(len(tmp_list)):
+    json_list.append(tmp_json_list[tmp_list[i][1]])
+
+  return json_list
 
 # Returns the pod qos
 def get_all_pod_qos(slo):
 
   IST = pytz.timezone('Asia/Calcutta')
 
-  # Initialize the loki address,  "http://192.168.49.2" is the minikube address
-  os.environ['LOKI_ADDR'] = "http://192.168.49.2"
-
-  # Query from loki and store in a json file, with limit being 1e12 and starting time as some time in 2006
-  os.system("logcli query '{app=\"events-collector\"}' --limit=1000000000000 --output=jsonl --from=2006-01-02T15:04:05.999999999Z --forward > ../logs/logs.json")
-
-  objlist = []
+  # Generate a file with logs of all the logs of the cluster
+  json_list = _generate_logs()
 
   # Stores timestamp, pod_names, actions of all events obtained by querying loki
   timestamps = []
   pod_names = []
   action = []
 
-
-  # Processing logs.json and extracting timestamps, pod_names, actions from them
-  with open('../logs/logs.json') as f:
-    for jsonObj in f:
-      temp = json.loads(jsonObj)
-      objlist.append(temp)
-
-  for obj in objlist:
+  for obj in json_list:
     timestamps.append(parser.parse(obj['timestamp']))
-    line = obj['line']
-    line = line.split()
-    action.append(line[2])
-    pod_names.append(line[3][4:])
+    action.append(obj['action'])
+    try:
+      pod_names.append(obj['involvedObject'])
+    except:
+      pod_names.append("none")
 
   # If we can't find creation_timestamp of some pod then assume it started running from default_time
   default_time = timestamps[0]
@@ -70,6 +112,7 @@ def get_all_pod_qos(slo):
   # Runtime and pause time of the deployments
   deployment_run_time = {}
   deployment_pause_time = {}
+  deployment_qos_metric = {}
   deployment_qos = {}
 
 
@@ -83,7 +126,7 @@ def get_all_pod_qos(slo):
     if pod_names[i] not in pod_creation:
       pod_creation[pod_names[i]] = default_time
     if pod_names[i] not in pod_deployment:
-      pod_deployment[pod_names[i]] = '_'
+      pod_deployment[pod_names[i]] = "_"
 
 
 
@@ -92,6 +135,7 @@ def get_all_pod_qos(slo):
     dep = pod_deployment[pod]
     deployment_run_time[dep] = 0.0
     deployment_pause_time[dep] = 0.0
+    deployment_qos_metric[dep] = 0.0
     deployment_qos[dep] = 0.0
 
 
@@ -110,7 +154,7 @@ def get_all_pod_qos(slo):
     # To be cautious, put it in try, except
     try:
       # Pulling and scheduled denotes that pod has been scheduled
-      if action[i] == 'Pulling' or action[i] == 'Scheduled':
+      if action[i] == "Pulling" or action[i] == "Scheduled":
         # If the pod is running for the first time then initialize some of it's values
         if pod_names[i] not in whether_created:
           whether_created[pod_names[i]] = 1
@@ -119,12 +163,12 @@ def get_all_pod_qos(slo):
 
         # If the pod changed it's state then update it's state and increment deployment_pause_time of it's deployment
         if pod_state[pod_names[i]] != 'Running':
-          deployment_pause_time[pod_deployment[pod_names[i]]] += time_difference(timestamps[i], pod_end_time[pod_names[i]])
+          deployment_pause_time[pod_deployment[pod_names[i]]] += (timestamps[i] - pod_end_time[pod_names[i]]).total_seconds()
           pod_start_time[pod_names[i]] = timestamps[i]
           pod_state[pod_names[i]] = 'Running'
 
       # SandboxChanged and Killing denotes that it has been put to the pending state
-      if action[i] == 'Killing' or action[i] == 'SandboxChanged':
+      if action[i] == "Killing" or action[i] == "SandboxChanged":
         # If the pod is running for the first time then initialize some of it's values
         if pod_names[i] not in whether_created:
           whether_created[pod_names[i]] = 1
@@ -133,7 +177,7 @@ def get_all_pod_qos(slo):
 
         # If the pod changed it's state then update it's state and increment deployment_run_time of it's deploment
         elif pod_state[pod_names[i]] != 'Paused':
-          deployment_run_time[pod_deployment[pod_names[i]]] += time_difference(timestamps[i],pod_start_time[pod_names[i]])
+          deployment_run_time[pod_deployment[pod_names[i]]] += (timestamps[i] - pod_start_time[pod_names[i]]).total_seconds()
           pod_end_time[pod_names[i]] = timestamps[i]
           pod_state[pod_names[i]] = 'Paused'
     except:
@@ -148,16 +192,16 @@ def get_all_pod_qos(slo):
     try:
       if pod not in pod_deletion:
         if pod_state[pod] == 'Running':
-          deployment_run_time[pod_deployment[pod]] += time_difference(datetime.datetime.now(tz = IST), pod_start_time[pod])
+          deployment_run_time[pod_deployment[pod]] += (datetime.datetime.now(tz = IST) - pod_start_time[pod]).total_seconds()
         else:
-          deployment_pause_time[pod_deployment[pod]] += time_difference(datetime.datetime.now(tz = IST), pod_end_time[pod])
+          deployment_pause_time[pod_deployment[pod]] += (datetime.datetime.now(tz = IST) - pod_end_time[pod]).total_seconds()
 
       else:
         if pod_state[pod] == 'Running':
-          deployment_run_time[pod_deployment[pod]] += time_difference(pod_deletion[pod], pod_start_time[pod])
+          deployment_run_time[pod_deployment[pod]] += (pod_deletion[pod] - pod_start_time[pod]).total_seconds()
 
         else:
-          deployment_pause_time[pod_deployment[pod]] += time_difference(pod_deletion[pod], pod_end_time[pod])
+          deployment_pause_time[pod_deployment[pod]] += (pod_deletion[pod] - pod_end_time[pod]).total_seconds()
     except:
       pass
 
@@ -171,13 +215,28 @@ def get_all_pod_qos(slo):
 
       if is_valid_pod(dep) == False:
         continue
-
-      deployment_qos[dep] = deployment_run_time[dep]/(deployment_slo[dep]+0.000000001) - (deployment_run_time[dep] + deployment_pause_time[dep])
+    
+      deployment_qos[dep] = deployment_run_time[dep]/(deployment_run_time[dep]+deployment_pause_time[dep]+0.000000001)
+      deployment_qos_metric[dep] = deployment_run_time[dep]/(deployment_slo[dep]+0.000000001) - (deployment_run_time[dep] + deployment_pause_time[dep])
+    
     except:
       pass
 
-  print("\n\nDeployment Qos = ", deployment_qos, "\n")
+  print("\n\nDeployment Qos = ", deployment_qos_metric, "\n")
   print("Run time = ", deployment_run_time, "\n")
   print("Pause time = ", deployment_pause_time, "\n")
 
-  return deployment_qos
+  dbfile = open('../files/deployment_run_time', 'wb')
+  pickle.dump(deployment_run_time, dbfile)
+  dbfile.close()
+
+  dbfile = open('../files/deployment_pause_time', 'wb')
+  pickle.dump(deployment_pause_time, dbfile)
+  dbfile.close()
+
+  dbfile = open('../files/deployment_qos', 'wb')
+  pickle.dump(deployment_qos, dbfile)
+  dbfile.close()
+
+
+  return deployment_qos_metric
