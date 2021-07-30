c4af83a940a30c1e56cd40310ebc8b2342e1e4b4
Akhilesh Sharma <47084013+Dragnoid99@users.noreply.github.com>
2021-07-30 17:16:45 +0530

    Update sample.sh

--- kubernetes/locust/locust13/sample.sh
+++ kubernetes/locust/locust13/sample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-locust -f locustfile.py --host http://192.168.49.2:30001/service-1-3/polls/ --headless #-u 20 -r 20
+locust -f locustfile.py --host http:///service-1-3/polls/ --headless #-u 20 -r 20
